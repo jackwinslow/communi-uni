a5d3ee8d5d6be42a72cdbfcd8e5a7aeb44dfcb81
Jack <securablesneakers@gmail.com>
2021-05-08 16:38:50 -0400

    Initial Commit

---
+++ Communi Uni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Communi Uni
+//
+//  Created by BC Swift Student Loan 1 on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
